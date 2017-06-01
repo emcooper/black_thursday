2c245f1060e62ff938d0d71be55535e545d7ca21
Desislava Todorova <dslavatodorova@gmail.com>
2017-06-01 13:42:00 -0600

    adds merchant repository class and methods with tests

---
+++ lib/merchant_repository.rb
@@ -0,0 +1,49 @@
+require 'pry'
+require "csv"
+require "./lib/merchant"
+
+
+class MerchantRepository
+    attr_reader :merchants
+
+  def initialize
+    @merchants = []
+  end
+
+  def from_csv(file_path)
+    contents = CSV.open file_path, headers: true, header_converters: :symbol
+    contents.each do |row|
+      attributes ={}
+      attributes[:id]           = row[:id].to_i
+      attributes[:name]         = row[:name]
+      @merchants << Merchant.new(attributes)
+    end
+  end
+
+  def all
+    @merchants
+  end
+
+  def find_by_id(id_number)
+    merchant = @merchants.find do |merchant|
+      merchant.id == id_number
+    end
+    return merchant
+  end
+
+  def find_by_name(name)
+    merchant = @merchants.find do |merchant|
+      merchant.name.downcase == name.downcase
+    end
+    return merchant
+  end
+
+  def find_all_by_name(keyword)
+    matching_merchants = @merchants.find_all do |merchant|
+      merchant.name.downcase.include?(keyword.downcase)
+    end
+    return [] if matching_merchants.nil?
+    return matching_merchants
+  end
+
+end
