bd76b6752e668c1e8afeb93c06a0fa60f4ed6811
Ellen Cooper <ellencooper74@gmail.com>
2017-06-02 07:51:40 -0600

    adds average_item_price_per_merchant_method

--- lib/sales_analyst.rb
+++ lib/sales_analyst.rb
@@ -18,5 +18,20 @@ class SalesAnalyst
   def items_per_merchant
     se.merchants.merchants.map {|merchant| merchant.items.count}
   end
+  
+  def average_item_price_per_merchant(merchant_id)
+    merchant = se.merchants.find_by_id(merchant_id)
+    sum = sum_of_item_prices(merchant)
+    number_of_items = merchant.items.count
+    average(sum, number_of_items)
+  end 
+  
+  def sum_of_item_prices(merchant)
+    merchant.items.reduce(0) {|sum, item| sum += item.unit_price}
+  end 
+  
+  def average(sum, number)
+    (sum/number).to_f.round(2)
+  end 
 
 end
--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -32,5 +32,34 @@ class SalesAnalystTest < Minitest::Test
     assert_instance_of Array, sa.items_per_merchant
     assert_equal 3, sa.items_per_merchant[0]
   end
+  
+  def test_items_per_merchant_returns_array_with_number_of_items
+    sa = SalesAnalyst.new(SalesEngine.from_csv({
+                            :items     => "./data/items.csv",
+                            :merchants => "./data/merchants.csv",
+                            }))
 
+    assert_instance_of Array, sa.items_per_merchant
+    assert_equal 3, sa.items_per_merchant[0]
+  end
+  
+  def test_average_item_price_per_merchant_returns_average
+    sa = SalesAnalyst.new(SalesEngine.from_csv({
+                            :items     => "./data/items.csv",
+                            :merchants => "./data/merchants.csv",
+                            }))
+    
+    assert_equal  42.5, sa.average_item_price_per_merchant(12334360)
+    assert_equal  16.66, sa.average_item_price_per_merchant(12334105)
+  end 
+  
+  def test_average_returns_rounded_average
+    sa = SalesAnalyst.new(SalesEngine.from_csv({
+                            :items     => "./data/items.csv",
+                            :merchants => "./data/merchants.csv",
+                            }))
+    
+    assert_equal  5, sa.average(15, 3)
+    assert_equal  20, sa.average(100, 5)
+  end 
 end
