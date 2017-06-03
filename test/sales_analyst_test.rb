55809a1bdf8ac6eb0802dc3f1f153604ea5c1cd6
Ellen Cooper <ellencooper74@gmail.com>
2017-06-02 19:51:33 -0600

    adds new_sales_analyst method to tests

--- test/sales_analyst_test.rb
+++ test/sales_analyst_test.rb
@@ -44,10 +44,7 @@ class SalesAnalystTest < Minitest::Test
   end
   
   def test_average_item_price_per_merchant_returns_big_decimal_average
-    sa = SalesAnalyst.new(SalesEngine.from_csv({
-                            :items     => "./data/items.csv",
-                            :merchants => "./data/merchants.csv",
-                            }))
+    sa = new_sales_analyst
     
     assert_equal  42.5, sa.average_item_price_per_merchant(12334360)
     assert_equal  16.66, sa.average_item_price_per_merchant(12334105)
@@ -55,20 +52,15 @@ class SalesAnalystTest < Minitest::Test
   end 
   
   def test_sum_of_item_prices_returns_sum
-    sa = SalesAnalyst.new(SalesEngine.from_csv({
-                            :items     => "./data/items.csv",
-                            :merchants => "./data/merchants.csv",
-                            }))
+    sa = new_sales_analyst 
+    
     merchant = sa.se.merchants.find_by_id(12334360)
     
     assert_equal  85, sa.sum_of_item_prices(merchant)
   end 
   
   def test_average_returns_rounded_big_decimal_average
-    sa = SalesAnalyst.new(SalesEngine.from_csv({
-                            :items     => "./data/items.csv",
-                            :merchants => "./data/merchants.csv",
-                            }))
+    sa = new_sales_analyst
     
     assert_equal  5, sa.average(15, 3)
     assert_equal  14.29, sa.average(100, 7)
@@ -76,29 +68,20 @@ class SalesAnalystTest < Minitest::Test
   end 
   
   def test_average_average_price_per_merchant_returns_bigdecimal_average
-    sa = SalesAnalyst.new(SalesEngine.from_csv({
-                            :items     => "./data/items.csv",
-                            :merchants => "./data/merchants.csv",
-                            }))
+    sa = new_sales_analyst
                             
     assert_equal  350.29, sa.average_average_price_per_merchant                        
     assert_instance_of BigDecimal, sa.average_average_price_per_merchant
   end 
   
   def test_sum_of_merchant_average_prices_returns_sum
-    sa = SalesAnalyst.new(SalesEngine.from_csv({
-                            :items     => "./data/items.csv",
-                            :merchants => "./data/merchants.csv",
-                            }))
+    sa = new_sales_analyst
                             
     assert_equal 166390.01, sa.sum_of_merchant_average_prices 
   end 
   
   def test_number_of_merchants_returns_merchant_count
-    sa = SalesAnalyst.new(SalesEngine.from_csv({
-                            :items     => "./data/items.csv",
-                            :merchants => "./data/merchants.csv",
-                            }))
+    sa = new_sales_analyst
                             
     assert_equal 475, sa.number_of_merchants 
   end 
