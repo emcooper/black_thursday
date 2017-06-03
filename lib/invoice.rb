344b5f63eb1eafee79721edc022eaf19d11c7acc
Ellen Cooper <ellencooper74@gmail.com>
2017-06-03 13:46:31 -0600

    adds Invoice class

---
+++ lib/invoice.rb
@@ -0,0 +1,15 @@
+class Invoice
+  attr_reader :id, :customer_id, :merchant_id, :status, :created_at, :updated_at, :repo
+
+  def initialize(attributes, repo)
+    @id           = attributes[:id]
+    @customer_id  = attributes[:customer_id]
+    @merchant_id  = attributes[:merchant_id]
+    @status       = attributes[:status]
+    @created_at   = attributes[:created_at]
+    @updated_at   = attributes[:updated_at]
+    @repo         = repo
+  end
+
+  
+end
---
+++ test/invoice_test.rb
@@ -0,0 +1,24 @@
+require_relative 'test_helper'
+require_relative '../lib/invoice'
+
+class InvoiceTest < Minitest::Test
+  def test_it_initializes_with_instance_variables
+    invoice = Invoice.new({
+      :id          => 20,
+      :customer_id => 1,
+      :merchant_id => 2,
+      :status      => "pending",
+      :created_at  => Time.new(2016, 10, 31),
+      :updated_at  => Time.new(2017, 10, 31)},
+      "repo_placeholder")
+
+    assert_equal 20, invoice.id
+    assert_equal 1, invoice.customer_id
+    assert_equal 2, invoice.merchant_id
+    assert_equal "pending", invoice.status
+    assert_instance_of Time, invoice.created_at
+    assert_instance_of Time, invoice.updated_at
+    assert_equal "repo_placeholder", invoice.repo
+  end
+  
+end 
