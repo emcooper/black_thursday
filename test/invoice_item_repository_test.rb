4cf4740c75428b9656a10460df743815601cd055
Desislava Todorova <dslavatodorova@gmail.com>
2017-06-04 17:47:24 -0600

    fixes typo in name file for invoice_item_repository file and fixes Time format test; adds def all in transaction_repository

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -3,13 +3,13 @@ require_relative '../lib/invoice_item_repository'
 
 class InvoiceItemRepositoryTest < Minitest::Test
   def test_it_initializes_items_with_empty_array
-    repo = InvoiceItemRepository.new("se_placeholder")
+    repo = InvoiceItemRepository.new(SalesEngine.new)
 
     assert_equal [], repo.invoice_items
   end
 
   def test_from_csv_adds_list_of_incoice_items_to_invoice_items
-    repo = InvoiceItemRepository.new("se_placeholder")
+    repo = InvoiceItemRepository.new(SalesEngine.new)
     repo.from_csv("./data/invoice_items_fixture.csv")
 
     assert_instance_of InvoiceItem, repo.invoice_items[0]
@@ -17,7 +17,7 @@ class InvoiceItemRepositoryTest < Minitest::Test
   end
 
   def test_item_attributes_are_correctly_formatted
-    repo = InvoiceItemRepository.new("se_placeholder")
+    repo = InvoiceItemRepository.new(SalesEngine.new)
     repo.from_csv("./data/invoice_items_fixture.csv")
 
     assert_instance_of Integer, repo.invoice_items[100].id
@@ -25,19 +25,19 @@ class InvoiceItemRepositoryTest < Minitest::Test
     assert_instance_of Integer, repo.invoice_items[100].invoice_id
     assert_instance_of Integer, repo.invoice_items[100].quantity
     assert_instance_of BigDecimal,repo.invoice_items[100].unit_price
-    assert_instance_of DateTime, repo.invoice_items[100].created_at
-    assert_instance_of DateTime, repo.invoice_items[100].updated_at
+    assert_instance_of Time, repo.invoice_items[100].created_at
+    assert_instance_of Time, repo.invoice_items[100].updated_at
   end
 
   def test_all_returns_array_of_all_invoice_item_instances
-    repo = InvoiceItemRepository.new("se_placeholder")
+    repo = InvoiceItemRepository.new(SalesEngine.new)
     repo.from_csv("./data/invoice_items_fixture.csv")
 
     assert_equal repo.invoice_items, repo.all
   end
 
   def test_find_by_id_returns_nil_or_matching_instance_of_invoice_id
-    repo = InvoiceItemRepository.new("se_placeholder")
+    repo = InvoiceItemRepository.new(SalesEngine.new)
     repo.from_csv("./data/invoice_items_fixture.csv")
 
     assert_equal 263454779, repo.find_by_id(2).item_id
@@ -45,7 +45,7 @@ class InvoiceItemRepositoryTest < Minitest::Test
   end
 
   def test_find_all_by_item_id_returns_empty_array_or_matching_invoice_items
-    repo = InvoiceItemRepository.new("se_placeholder")
+    repo = InvoiceItemRepository.new(SalesEngine.new)
     repo.from_csv("./data/invoice_items_fixture.csv")
 
     assert_equal 1, repo.find_all_by_item_id(263519844)[0].id
@@ -53,7 +53,7 @@ class InvoiceItemRepositoryTest < Minitest::Test
   end
 
   def test_find_all_by_invoice_id_returns_empty_array_or_matching_invoice_items
-    repo = InvoiceItemRepository.new("se_placeholder")
+    repo = InvoiceItemRepository.new(SalesEngine.new)
     repo.from_csv("./data/invoice_items_fixture.csv")
 
     assert_equal 23 , repo.find_all_by_invoice_id(5)[0].id
--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -5,17 +5,27 @@ require_relative "../lib/transaction_repository"
 class TransactionRepositoryTest < Minitest::Test
 
   def test_it_initializes_transactions_with_empty_array
-    tr = TransactionRepository.new("se_placeholder")
+    tr = TransactionRepository.new(SalesEngine.new)
 
     assert_equal [], tr.transactions
   end
 
   def test_it_initializes_with_array_of_transactions
-    tr = TransactionRepository.new("se_placeholder")
+    tr = TransactionRepository.new(SalesEngine.new)
     tr.from_csv("./data/transactions.csv")
 
     assert_instance_of Array, tr.transactions
     assert_instance_of Transaction, tr.transactions.first
+
+  end
+
+  def test_all_returns_array_with_transaction_objects
+    tr = TransactionRepository.new(SalesEngine.new)
+    tr.from_csv("./data/transactions.csv")
+
+    assert_instance_of Array, tr.all
+    assert_instance_of Transaction, tr.all[2]
+    
 
   end
 
