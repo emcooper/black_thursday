2fec36276b5b9d05bbfb9687f9566789cdd27c78
Desislava Todorova <dslavatodorova@gmail.com>
2017-06-04 15:43:44 -0600

    adds transactions and invoice_items repos to sales engine

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -3,12 +3,14 @@ require_relative 'item_repository'
 require_relative 'invoice_repository'
 
 class SalesEngine
-  attr_reader :merchants, :items, :invoices
+  attr_reader :merchants, :items, :invoices, :transactions, :invoice_items
 
   def initialize
     @merchants = MerchantRepository.new(self)
     @items     = ItemRepository.new(self)
     @invoices  = InvoiceRepository.new(self)
+    @transactions = TransactionRepository.new(self)
+    @invoice_items = InvoiceItemRepository.new(self)
   end
 
   def self.from_csv(csv_file_locations)
@@ -16,6 +18,8 @@ class SalesEngine
     se.merchants.from_csv(csv_file_locations[:merchants]) if csv_file_locations[:merchants]
     se.items.from_csv(csv_file_locations[:items]) if csv_file_locations[:items]
     se.invoices.from_csv(csv_file_locations[:invoices]) if csv_file_locations[:invoices]
+    se.transactions.from_csv(csv_file_locations[:transactions]) if csv_file_locations[:transactions]
+    se.invoice_items.from_csv(csv_file_locations[:invoice_items]) if csv_file_locations[:invoice_items]
     return se
   end
 end
