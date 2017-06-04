aa2d8469ca6d6876f38f02862bcaf99721e33eee
Desislava Todorova <dslavatodorova@gmail.com>
2017-06-04 09:04:02 -0600

    creates files structure for transaction/transaction_repository and adds transaction class with initialize and test

---
+++ test/transactions_test.rb
@@ -0,0 +1,27 @@
+require_relative 'test_helper'
+require_relative '../lib/transactions'
+
+
+class TransactionTest < Minitest::Test
+
+  def test_it_initializes_with_instance_variables
+    t = Transaction.new({
+       :id => 6,
+       :invoice_id => 8,
+       :credit_card_number => "4242424242424242",
+       :credit_card_expiration_date => "0220",
+       :result => "success",
+       :created_at => Time.now,
+       :updated_at => Time.now
+     }, "repo_placeholder")
+
+     assert_equal 6, t.id
+     assert_equal 8, t.invoice_id
+     assert_equal "4242424242424242", t.credit_card_number
+     assert_equal "0220", t.credit_card_expiration_date
+     assert_equal "success", t.result
+     assert_instance_of Time, t.created_at
+     assert_instance_of Time, t.updated_at
+  end
+
+end
