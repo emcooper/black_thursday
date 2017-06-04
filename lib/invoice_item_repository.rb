0adfd17a5183d48e2be6328f60f56d8496717785
Desislava Todorova <dslavatodorova@gmail.com>
2017-06-03 18:33:24 -0600

    adds InvoiceItemRepository methods and tests

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -26,4 +26,34 @@ class InvoiceItemRepository
       @invoice_items << InvoiceItem.new(attributes, self)
     end
   end
+
+  def all
+    @invoice_items
+  end
+
+  def find_by_id(id)
+    matching_item = @invoice_items.find do |invoice_item|
+      invoice_item.id == id
+    end
+    return matching_item
+  end
+
+  def find_all_by_item_id(item_id)
+    matching_items = @invoice_items.find_all do |invoice_item|
+      invoice_item.item_id == item_id
+    end
+    return [] if matching_items.nil?
+    return matching_items
+
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+    matching_items =  @invoice_items.find_all do |invoice_item|
+      invoice_item.invoice_id == invoice_id
+    end
+    return [] if matching_items.nil?
+    return matching_items
+  end
+
+
 end
--- test/invoice_item_repositoy_test.rb
+++ test/invoice_item_repositoy_test.rb
@@ -10,7 +10,7 @@ class InvoiceItemRepositoryTest < Minitest::Test
 
   def test_from_csv_adds_list_of_incoice_items_to_invoice_items
     repo = InvoiceItemRepository.new("se_placeholder")
-    repo.from_csv("./data/invoice_items.csv")
+    repo.from_csv("./data/invoice_items_fixture.csv")
 
     assert_instance_of InvoiceItem, repo.invoice_items[0]
     assert_instance_of InvoiceItem, repo.invoice_items[100]
@@ -18,7 +18,7 @@ class InvoiceItemRepositoryTest < Minitest::Test
 
   def test_item_attributes_are_correctly_formatted
     repo = InvoiceItemRepository.new("se_placeholder")
-    repo.from_csv("./data/invoice_items.csv")
+    repo.from_csv("./data/invoice_items_fixture.csv")
 
     assert_instance_of Integer, repo.invoice_items[100].id
     assert_instance_of Integer, repo.invoice_items[100].item_id
@@ -27,6 +27,38 @@ class InvoiceItemRepositoryTest < Minitest::Test
     assert_instance_of BigDecimal,repo.invoice_items[100].unit_price
     assert_instance_of DateTime, repo.invoice_items[100].created_at
     assert_instance_of DateTime, repo.invoice_items[100].updated_at
+  end
+
+  def test_all_returns_array_of_all_invoice_item_instances
+    repo = InvoiceItemRepository.new("se_placeholder")
+    repo.from_csv("./data/invoice_items_fixture.csv")
+
+    assert_equal repo.invoice_items, repo.all
+  end
+
+  def test_find_by_id_returns_nil_or_matching_instance_of_invoice_id
+    repo = InvoiceItemRepository.new("se_placeholder")
+    repo.from_csv("./data/invoice_items_fixture.csv")
+
+    assert_equal 263454779, repo.find_by_id(2).item_id
+    assert_nil repo.find_by_id(1000)
+  end
+
+  def test_find_all_by_item_id_returns_empty_array_or_matching_invoice_items
+    repo = InvoiceItemRepository.new("se_placeholder")
+    repo.from_csv("./data/invoice_items_fixture.csv")
+
+    assert_equal 1, repo.find_all_by_item_id(263519844)[0].id
+    assert_equal [], repo.find_all_by_item_id(000)
+  end
+
+  def test_find_all_by_invoice_id_returns_empty_array_or_matching_invoice_items
+    repo = InvoiceItemRepository.new("se_placeholder")
+    repo.from_csv("./data/invoice_items_fixture.csv")
+
+    assert_equal 23 , repo.find_all_by_invoice_id(5)[0].id
+    assert_instance_of Array, repo.find_all_by_invoice_id(5)
+    assert_equal [], repo.find_all_by_invoice_id(000)
   end
 
 end
