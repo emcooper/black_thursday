1e6616120545b44238f3800d60b668db94bd7c11
Desislava Todorova <dslavatodorova@gmail.com>
2017-06-05 13:06:27 -0600

    adds methods to transaction repo and tests; attribute type conversion in transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -4,9 +4,9 @@ class Transaction
   attr_reader :id, :invoice_id, :credit_card_number, :credit_card_expiration_date, :result, :created_at, :updated_at, :repo
 
   def initialize(attributes, repo)
-    @id                          = attributes[:id]
-    @invoice_id                  = attributes[:invoice_id]
-    @credit_card_number          = attributes[:credit_card_number]
+    @id                          = attributes[:id].to_i
+    @invoice_id                  = attributes[:invoice_id].to_i
+    @credit_card_number          = attributes[:credit_card_number].to_i
     @credit_card_expiration_date = attributes[:credit_card_expiration_date]
     @result                      = attributes[:result]
     @created_at                  = attributes[:created_at]
--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -7,16 +7,15 @@ class TransactionRepositoryTest < Minitest::Test
   def test_it_initializes_transactions_with_empty_array
     tr = TransactionRepository.new(SalesEngine.new)
 
-    assert_equal [], tr.transactions
+    assert_equal [], tr.all
   end
 
   def test_it_initializes_with_array_of_transactions
     tr = TransactionRepository.new(SalesEngine.new)
     tr.from_csv("./data/transactions.csv")
 
-    assert_instance_of Array, tr.transactions
-    assert_instance_of Transaction, tr.transactions.first
-
+    assert_instance_of Array, tr.all
+    assert_instance_of Transaction, tr.all.first
   end
 
   def test_all_returns_array_with_transaction_objects
@@ -25,11 +24,41 @@ class TransactionRepositoryTest < Minitest::Test
 
     assert_instance_of Array, tr.all
     assert_instance_of Transaction, tr.all[2]
-    
-
   end
 
+  def test_find_by_id_returns_nil_or_instance_of_transaction
+    tr = TransactionRepository.new(SalesEngine.new)
+    tr.from_csv("./data/transactions.csv")
 
+    assert_instance_of Transaction, tr.find_by_id(1)
+    assert_nil tr.find_by_id(000)
+  end
+
+  def test_find_all_by_invoice_id_returns_empty_array_or_matching_transactions
+    tr = TransactionRepository.new(SalesEngine.new)
+    tr.from_csv("./data/transactions.csv")
+
+    assert_equal      2,   tr.find_all_by_invoice_id(2179).count
+    assert_equal     767,  tr.find_all_by_invoice_id(2179)[1].id
+    assert_equal      [], tr.find_all_by_invoice_id(0000)
+  end
+
+  def test_find_all_by_credit_card_number_returns_empty_array_or_matching_transactions
+    tr = TransactionRepository.new(SalesEngine.new)
+    tr.from_csv("./data/transactions.csv")
+
+    assert_equal [],   tr.find_all_by_credit_card_number(40686319432314730)
+    assert_equal 2179, tr.find_all_by_credit_card_number(4068631943231473)[0].invoice_id
+    assert_equal 1,    tr.find_all_by_credit_card_number(4068631943231473).count
+  end
+
+  def test_find_all_by_result_returns_empty_array_or_matching_transactions
+    tr = TransactionRepository.new(SalesEngine.new)
+    tr.from_csv("./data/transactions.csv")
+
+    assert_equal [], tr.find_all_by_result("wrong")
+    assert_equal 827, tr.find_all_by_result("failed").count
+  end
 
 
 end
