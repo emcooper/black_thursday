618616afddadbf621761ee0fd7d7418f6e4a03ad
Desislava Todorova <dslavatodorova@gmail.com>
2017-06-07 20:15:29 -0600

    completes sanitation requests for sales_engine and completes se test

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -6,7 +6,8 @@ require_relative 'invoice_item_repository'
 require_relative 'customer_repository'
 
 class SalesEngine
-  attr_reader :merchants, :items, :invoices, :transactions, :invoice_items, :customers
+  attr_reader :merchants, :items, :invoices, :transactions, :invoice_items,
+              :customers
 
   def initialize
     @merchants = MerchantRepository.new(self)
@@ -17,14 +18,14 @@ class SalesEngine
     @customers = CustomerRepository.new(self)
   end
 
-  def self.from_csv(csv_file_locations)
+  def self.from_csv(csv)
     se = SalesEngine.new
-    se.merchants.from_csv(csv_file_locations[:merchants]) if csv_file_locations[:merchants]
-    se.items.from_csv(csv_file_locations[:items]) if csv_file_locations[:items]
-    se.invoices.from_csv(csv_file_locations[:invoices]) if csv_file_locations[:invoices]
-    se.transactions.from_csv(csv_file_locations[:transactions]) if csv_file_locations[:transactions]
-    se.invoice_items.from_csv(csv_file_locations[:invoice_items]) if csv_file_locations[:invoice_items]
-    se.customers.from_csv(csv_file_locations[:customers]) if csv_file_locations[:customers]
+    se.merchants.from_csv(csv[:merchants]) if csv[:merchants]
+    se.items.from_csv(csv[:items]) if csv[:items]
+    se.invoices.from_csv(csv[:invoices]) if csv[:invoices]
+    se.transactions.from_csv(csv[:transactions]) if csv[:transactions]
+    se.invoice_items.from_csv(csv[:invoice_items]) if csv[:invoice_items]
+    se.customers.from_csv(csv[:customers]) if csv[:customers]
     return se
   end
 end
--- test/sales_engine_test.rb
+++ test/sales_engine_test.rb
@@ -21,12 +21,19 @@ class SalesEngineTest < Minitest::Test
 
   def test_from_csv_populates_repositories
     se = SalesEngine.from_csv({
-                            :items     => "./data/items.csv",
-                            :merchants => "./data/merchants.csv",
-                            :invoices  => "test/data/it-2/invoices.csv"
+                            :items         => "./data/items.csv",
+                            :merchants     => "./data/merchants.csv",
+                            :invoices      => "test/data/it-2/invoices.csv",
+                            :invoice_items => "test/data/it-4/invoice_items.csv",
+                            :transactions  => "test/data/it-4/transactions.csv",
+                            :customers     => "test/data/it-4/customers.csv"
                             })
-    assert_instance_of Merchant, se.merchants.all.sample
-    assert_instance_of Item, se.items.all.sample
+
+    assert_instance_of Merchant,se.merchants.all.sample
+    assert_instance_of Item,    se.items.all.sample
     assert_instance_of Invoice, se.invoices.all.sample
+    assert_instance_of InvoiceItem, se.invoice_items.all.sample
+    assert_instance_of Transaction, se.transactions.all.sample
+    assert_instance_of Customer, se.customers.all.sample
   end
 end
